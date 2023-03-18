96c3f756148d6d04e06eaac74962ae67ceb996fb
kliko <kliko@softalix.com>
2023-03-19 01:05:17 +0200

    Asynchronous Programming - Bus stop

--- 01_asynchronous-programming/01.Bus-Stop/app.js
+++ 01_asynchronous-programming/01.Bus-Stop/app.js
@@ -1,3 +1,76 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById('stopId').value;
+    const stopNameWrapper = document.getElementById('stopName');
+    const busesListWrapper = document.getElementById('buses');
+    const API_ENDPOINT = 'http://localhost:3030';
+
+    function createElement({type, attr, content}) {
+        const elm = document.createElement(type);
+        Object.assign(elm, attr);
+
+        if (content) {
+            elm.appendChild(content);
+        }
+
+        return elm;
+    }
+
+    async function makeRequest(url) {
+        return await fetch(`${API_ENDPOINT}/${url}`);
+    }
+
+    async function getBusStopData(stopId) {
+        return (await makeRequest(`jsonstore/bus/businfo/${stopId}`))
+            .json();
+    }
+
+    function setBusStopName(name) {
+        stopNameWrapper.textContent = name;
+    }
+
+    function setBusesList(buses) {
+        Object.entries(buses)
+            .forEach(([id, time]) => {
+                busesListWrapper.appendChild(createElement({
+                    type: 'li',
+                    attr: {
+                        textContent: `Bus ${id} arrives in ${time} minutes`
+                    }
+                }));
+            });
+    }
+
+    function clear() {
+        setBusStopName('')
+        busesListWrapper.innerHTML = '';
+    }
+
+    clear();
+
+    try {
+        const data = await getBusStopData(stopId);
+        if (!data) {
+            throw new Error('No data');
+        }
+
+        if (!data.name) {
+            throw new Error('No name');
+        }
+
+        if (!data.buses) {
+            Object.keys(data.buses)
+                .forEach(k => {
+                    if (isNaN(k) || isNaN(data.buses[k])) {
+                        throw new Error('Invalid');
+                    }
+                })
+
+            throw new Error('No buses');
+        }
+
+        setBusStopName(data.name);
+        setBusesList(data.buses);
+    } catch (e) {
+        setBusStopName('Error')
+    }
 }
